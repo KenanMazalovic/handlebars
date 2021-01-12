932658b4aa3420aaaaac83bd0b1ba4217b93d6b7
Hajdarevic Haris <hajdarevich@gmail.com>
2021-01-12 11:10:14 +0100

    Handlebars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HtmlHandlebarsComponent } from './html-handlebars/html-handlebars.component';
+import {SafeHtmlPipe} from './pipes/safeHtml.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     DynamicFormComponent,
-    HtmlHandlebarsComponent
+    HtmlHandlebarsComponent,
+    SafeHtmlPipe
   ],
   imports: [
     BrowserModule,
